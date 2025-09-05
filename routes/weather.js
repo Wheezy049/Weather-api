e0fa043b9eb478385c07672fd857de7976766057
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-09-05 16:24:37 +0100

    add swagger docs

--- routes/weather.js
+++ routes/weather.js
@@ -6,8 +6,89 @@ const {
 
 const weatherRouter = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Weather
+ *   description: Weather data endpoints
+ */
+
+/**
+ * @swagger
+ * /api/weather/current/{city}:
+ *   get:
+ *     summary: Get current weather by city
+ *     tags: [Weather]
+ *     parameters:
+ *       - in: path
+ *         name: city
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the city
+ *     responses:
+ *       200:
+ *         description: Current weather data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 city:
+ *                   type: string
+ *                   example: London
+ *                 temperature:
+ *                   type: number
+ *                   example: 22.5
+ *                 condition:
+ *                   type: string
+ *                   example: Sunny
+ *       404:
+ *         description: City not found
+ */
 weatherRouter.get("/current/:city", getCurrentWeather);
 
+/**
+ * @swagger
+ * /api/weather/forecast/{city}:
+ *   get:
+ *     summary: Get 5-day weather forecast by city
+ *     tags: [Weather]
+ *     parameters:
+ *       - in: path
+ *         name: city
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the city
+ *     responses:
+ *       200:
+ *         description: Weather forecast data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 city:
+ *                   type: string
+ *                   example: Paris
+ *                 forecast:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       date:
+ *                         type: string
+ *                         example: 2025-09-05
+ *                       temperature:
+ *                         type: number
+ *                         example: 20.3
+ *                       condition:
+ *                         type: string
+ *                         example: Rainy
+ *       404:
+ *         description: City not found
+ */
 weatherRouter.get("/forecast/:city", getWeatherForecast);
 
 module.exports = weatherRouter;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc")
 const weatherRouter = require("./routes/weather");
 const PORT = 3000;
 
@@ -7,8 +9,28 @@ const app = express();
 
 app.use(express.json());
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My Weather API",
+      version: "1.0.0",
+      description: "API documentation with Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js", "./routes/*.ts"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Weather API is running");
 });
 
 app.use("/api/weather", weatherRouter);
